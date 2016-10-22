927b3dbee6d4caa689186ae7bd0b183e9ccf4ecd
Felix Dacumos <felix@dacumos.com>
2016-10-22 15:26:55 -0700

    Updating files with ec2 files

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'bitnami_wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'bn_wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '8d2224a5dc');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -46,14 +46,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY', '70199eee379b7f220161885a7ebbbdaaa090683b614ccfc0d954d9a89264ae76');
+define('SECURE_AUTH_KEY', '7241ddc4747440e72fe02a44f89d10af7d704e65d657c4ad1aa4d3187d127b7d');
+define('LOGGED_IN_KEY', '754a94479c636f085771610e0efd42d882be15c011d71ad0d339defdf1945bee');
+define('NONCE_KEY', 'd94b8c2ab52ae42a41a9a8a62cabc8cf3b9a61d2e78eeccc2114b7befc9af3f6');
+define('AUTH_SALT', '8aab6b208120f5986d645b901839dab386f183f67d34ceb0d34aea11c99366b3');
+define('SECURE_AUTH_SALT', '6a11b2221afab36569ee8c54d4fd7fcf854ae577bfa34fc7f035bee2d27e5309');
+define('LOGGED_IN_SALT', 'd09981f02c65087468a1107066bd8f0336d481431437972e72e343949d4914d0');
+define('NONCE_SALT', '911835a102f5a7efb6017d3e3e9c0bcf090dc5042ebd3f5e63754aa9924568fd');
 
 /**#@-*/
 
@@ -80,6 +80,17 @@ $table_prefix  = 'wp_';
 define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
+/**
+ * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
+ * If you want to access only from an specific domain, you can modify them. For example:
+ *  define('WP_HOME','http://example.com');
+ *  define('WP_SITEURL','http://example.com');
+ *
+*/
+
+define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
@@ -87,3 +98,29 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_TEMP_DIR', '/opt/bitnami/apps/wordpress/tmp');
+
+
+define('FS_METHOD', 'ftpext');
+define('FTP_BASE', '/opt/bitnami/apps/wordpress/htdocs/');
+define('FTP_USER', 'bitnamiftp');
+define('FTP_PASS', 'mcFJEC5n7WwegEtgLMxWoqRMgOcXd8jzALy8YQuiu6JNyG5hJY');
+define('FTP_HOST', '127.0.0.1');
+define('FTP_SSL', false);
+
+
+
+//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
+//  More info at: https://wiki.bitnami.com/Applications/Bitnami_Wordpress#XMLRPC_and_Pingback
+
+// remove x-pingback HTTP header
+add_filter('wp_headers', function($headers) {
+    unset($headers['X-Pingback']);
+    return $headers;
+});
+// disable pingbacks
+add_filter( 'xmlrpc_methods', function( $methods ) {
+        unset( $methods['pingback.ping'] );
+        return $methods;
+});
